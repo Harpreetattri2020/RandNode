c6fe953d1c4f531aae66ef872a91963bdc75f0b3
harpreetattri2020 <Harpreetattri2020@gmail.com>
2021-10-10 18:22:32 +0100

    Node routes setups

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+
 const express = require("express")
 const app = express()
-const port = 4000
+const port = 3000
 const cors = require("cors")
+const fetch =require("node-fetch");
 
-// npm init
-// npm i express cors nodemon
-// they add a handy req.body object to our req,
-// containing a Javascript
-//  object representing the payload sent with the request
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
-app.get("/", cors(), async (req, res) => {
-	res.send("This is working")
-})
-app.get("/home", cors(), async (req, res) => {
-	res.send("This is the data for the home page")
+
+//Get all Users
+app.get("/", async(req,res)=>{
+
+    try{
+    const response = await fetch('https://coding-challenge-api.bounceinsights.com/users', {headers:{'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBsaWNhbnRJZCI6IlNweTBLUWFqcjliWWM0c1pvRUt6IiwiaWF0IjoxNjMzNTE4MzgwfQ.sYoCK4L_GpjBU2DZtQA1aOHbyWWQl398TnQZau4TSO8'}})
+    const data = await response.json();
+    res.status(200).json(data);
+    console.log(data);
+    }
+    catch(error){
+        res.status(500).json("Check the path");
+    }
 })
 
 app.post("/post_name", async (req, res) => {
